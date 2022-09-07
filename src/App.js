49a0e2d31b40d975ee90f7a40435d459ba2c8f89
SvetaMyasoedova <so.myasoedova@gmail.com>
2022-09-07 21:20:52 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import logo from './giphy.gif';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='first'>It's my first react app</h1>
+      <img className='gif' src={logo} alt="Wow" />
+    </div>
+  );
+}
+
+export default App;
